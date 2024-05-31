2095abf5643d037c885332bded147faab00dd31e
chlsnyhmhrn <chelseaanayah85@gmail.com>
2024-06-01 00:33:11 +0700

    Praktikum 11

--- Program Files/praktikum016/routes/web.php
+++ Program Files/praktikum016/routes/web.php
@@ -1,8 +1,13 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\controllers\ListProdukController;
+use App\Http\Controllers\ApiController;
+
 
 Route::get('/listproduk', [ListProdukController::class, 'show']);
 Route::post('/listproduk/action', [ListProdukController::class, 'simpan'])->name('produk.simpan');
 Route::delete('/listproduk/{id}', [ListProdukController::class, 'delete'])->name('produk.delete');
+route::get('api/produk', [ApiController::class, 'index']);
+route::get('api/list', [ApiController::class, 'getProduct']);
